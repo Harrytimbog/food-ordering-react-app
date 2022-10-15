357a77b55c86e76f8389dc37322cf6b7390d2eb0
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-15 13:38:56 +0100

    Adding Better User Feedback

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
@@ -7,6 +7,9 @@ import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+  const [httpError, setHttpError] = useState();
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -23,17 +26,30 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
-  const submitOrderHandler = (userData) => {
-    fetch(
-      `https://react-foodordering-app-a95e5-default-rtdb.firebaseio.com/orders.json`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          user: userData,
-          orderedItem: cartCtx.items,
-        }),
+  const submitOrderHandler = async (userData) => {
+    try {
+      setIsSubmitting(true);
+      const response = await fetch(
+        `https://react-foodordering-app-a95e5-default-rtdb.firebaseio.com/orders.json`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            user: userData,
+            orderedItem: cartCtx.items,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
       }
-    );
+
+      setIsSubmitting(false);
+      setDidSubmit(true);
+      cartCtx.clearCart();
+    } catch (error) {
+      setHttpError(error.message);
+    }
   };
 
   const cartItems = (
@@ -64,8 +80,8 @@ const Cart = (props) => {
     </div>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContent = (
+    <Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -75,6 +91,37 @@ const Cart = (props) => {
         <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
       )}
       {!isCheckout && modalActions}
+    </Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+
+  const submitError = (
+    <Fragment>
+      <section className={classes.submitError}>
+        <p>{httpError}</p>
+        <p>Please try again later...</p>
+      </section>
+    </Fragment>
+  );
+
+  const didSubmitModalContent = (
+    <Fragment>
+      <p>Successfully sent the order!</p>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
+      {httpError && submitError}
     </Modal>
   );
 };
